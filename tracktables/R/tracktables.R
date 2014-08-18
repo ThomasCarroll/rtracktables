d90d30daa0668719b4952204020b8ee0e8c4f828
Thomas Carroll <tc.infomatics@gmail.com>
2014-08-18 09:46:21 +0100

    Move peakTransform over to R directory

--- tracktables/R/tracktables.R
+++ tracktables/R/tracktables.R
@@ -29,7 +29,7 @@
 #' @return ChIPprofile A ChIPprofile object. 
 #' @export
 #' @import IRanges GenomicRanges ggplot2 QuasR rtracklayer GenomicAlignments GenomicRanges XVector Rsamtools reshape2
-#' @include allClasses.r plots.R
+#' @include allClasses.r plots.R peakTransforms.r
 regionPlot <- function(bamFile,testRanges,nOfWindows=100,FragmentLength=150,style="point",distanceAround=1500,distanceInRegionStart=1500,distanceOutRegionStart=1500,distanceInRegionEnd=1500,distanceOutRegionEnd=1500,paired=F,normalize="RPM",plotBy="coverage",removeDup=F,verbose=T,format="bam",seqlengths=NULL,forceFragment=NULL){
   if(!verbose){
     suppressMessages(runRegionPlot())
